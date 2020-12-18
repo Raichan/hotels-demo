85b85cb56fcbf9921e14a393c3f736ed09d6f37f
Laura Sirola <laura.m.sirola@gmail.com>
2020-12-18 23:12:55 +0200

    Filtering

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -5,7 +5,10 @@ import jsonData from "../hotels.json";
 
 const Hotels = () => {
   const [hotelList, setHotelList] = useState(jsonData);
-  const locations = ["Finland", "Spain", "Netherlands"];
+  const [locations, setLocations] = useState([]);
+  const [filterResults, setFilterResults] = useState(hotelList);
+  const [locationFilter, setLocationFilter] = useState("");
+  const [showAvailableOnly, setShowAvailableOnly] = useState(false);
 
   useEffect(() => {
     let newHotelList = hotelList;
@@ -15,21 +18,40 @@ const Hotels = () => {
       }
     });
     setHotelList(newHotelList);
+
+    setLocations([...new Set(hotelList.map((hotel) => hotel.country))]);
   }, [hotelList]);
+
+  useEffect(() => {
+    let newResults = hotelList;
+    if (locationFilter !== "") {
+      newResults = newResults.filter(
+        (hotel) => hotel.country === locationFilter
+      );
+    }
+    if (showAvailableOnly) {
+      newResults = newResults.filter((hotel) => hotel.isAvailable);
+    }
+    setFilterResults(newResults);
+  }, [hotelList, locationFilter, showAvailableOnly]);
 
   return (
     <div>
       <header>
-        <select>
+        <select onChange={(e) => setLocationFilter(e.target.value)}>
           <option value="" />
           {locations.map((location) => {
             return <option key={location}>{location}</option>;
           })}
         </select>
-        <input type="checkbox" /> Is available
+        <input
+          type="checkbox"
+          onChange={(e) => setShowAvailableOnly(e.target.checked)}
+        />{" "}
+        Is available
       </header>
       <main>
-        {hotelList.map((hotel) => {
+        {filterResults.map((hotel) => {
           return (
             <HotelInfo key={hotel.id} hotel={hotel} showReserveButton={true} />
           );
